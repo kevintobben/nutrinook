fe4fbaa2d4f8c7ef27401e8d108349b3d504acc8
Kevin Tobben <kevintobben@outlook.com>
2025-06-13 15:21:21 +0200

    daisyUi light theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     {
       title: "Spaghetti Carbonara",
       description: "Klassieke Italiaanse pasta met spek, ei en kaas. Een simpel maar heerlijk gerecht.",
-      image: "/images/carbonara.jpg",
+      image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
       cookingTime: "20 min",
       difficulty: "Gemakkelijk",
       servings: 4
@@ -15,11 +15,19 @@ function Home() {
     {
       title: "Chicken Tikka Masala",
       description: "Romige Indiase curry met gemarineerde kip in een rijke tomatensaus.",
-      image: "/images/tikka-masala.jpg",
+      image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
       cookingTime: "45 min",
       difficulty: "Gemiddeld",
       servings: 6
-    }
+    },
+    {
+      title: "Vegetarische Chili",
+      description: "Heerlijke en vullende chili met bonen, groenten en specerijen. Perfect voor een koude dag.",
+      image: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+      cookingTime: "30 min",
+      difficulty: "Gemakkelijk",
+      servings: 4
+    },
   ];
 
   const handleViewRecipe = (recipe) => {
